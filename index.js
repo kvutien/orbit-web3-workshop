29b80834952ad883569b5b54e785e2811c7142d6
kvutien <kvutien@VTKT5.fritz.box>
2021-06-21 15:23:13 +0200

    add 'explore' badge to README

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class App extends React.Component{
         preload: { "enabled": false },
         config: {
           Addresses: {
-            Swarm: ["/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star"]
+            Swarm: [/* "/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star" */]
           }
         }
       })
